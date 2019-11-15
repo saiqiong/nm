aba02cf79309c35b4715942609fd22707a770ba2
Saiqiong XIAO <saxiao@e1r3p10.42.fr>
2019-11-15 18:51:28 +0100

    init

---
+++ nm.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <mach-o/loader.h>
+#include <mach-o/nlist.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+void    print_output(int nsyms, int symoff, int stroff, char *ptr)
+{
+    int             i;
+    char            *stringtable;
+    struct nlist_64 *array;
+
+    array = (void *)ptr + symoff;
+    stringtable = (void *)ptr + stroff;
+    i = -1;
+    while (++i < nsyms) {
+        printf("%s\n", stringtable + array[i].n_un.n_strx);
+    }
+}
+
+void    handle_64(char *ptr)
+{
+    int                     ncmds;
+    struct mach_header_64   *header;
+    struct load_command     *lc;
+    int                     i;
+    struct symtab_command   *sym;
+    
+
+    header = (struct mach_header_64 *)ptr;
+    ncmds = header->ncmds;
+    lc = (void *)ptr + sizeof(*header);
+    i = -1;
+    while (++i < ncmds) {
+        if (lc->cmd == LC_SYMTAB) {
+            sym = (struct symtab_command *)lc;
+            print_output(sym->nsyms, sym->symoff, sym->stroff, ptr);
+            break;
+        }
+        lc = (void *)lc + lc->cmdsize;
+    }
+    
+
+}
+
+void    nm(char *ptr)
+{
+    int magic_number;
+
+    magic_number = *(int *)ptr;
+    if (magic_number == MH_MAGIC_64) {
+        handle_64(ptr);
+    }
+}
+
+int     main(int ac, char **av)
+{
+    int     fd;
+    char    *ptr;
+    struct stat buf;
+
+    if (ac !=2) {
+        fprintf(stderr, "Please give me an arg\n");
+        return (EXIT_FAILURE);
+    }
+    if ((fd = open(av[1], O_RDONLY)) < 0){
+        perror("open");
+        return (EXIT_FAILURE);
+    }
+    if (fstat(fd, &buf) < 0) {
+        perror("fstat");
+        return (EXIT_FAILURE);
+    }
+    if ((ptr = mmap(0, buf.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED) {
+        perror("mmap");
+        return (EXIT_FAILURE);
+    }
+    nm(ptr);
+    if (munmap(ptr, buf.st_size) < 0) {
+        perror("munmap");
+        return (EXIT_FAILURE);
+    }
+    return (EXIT_SUCCESS);
+}
